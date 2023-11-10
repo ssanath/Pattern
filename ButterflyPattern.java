2ed5aee813bd17907e70be6ea0b54450261d78da
ssanath <144911352+ssanath@users.noreply.github.com>
2023-11-10 15:16:28 +0530

    Update ButterflyPattern.java

--- ButterflyPattern.java
+++ ButterflyPattern.java
@@ -2,7 +2,7 @@ public class ButterflyPattern {
 	public static void main(String args[])
 	{
 		int r=8,c=8;
-        int spaces = c-2;
+                int spaces = c-2;
         for(int i=1;i<=r/2;i++)
         {
             for(int k=1;k<=i;k++)
